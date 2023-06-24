de0deb0962bc58f543eb63c1d0fcc8341c469a74
MuriloMoscardi <murilomoscardi@gmail.com>
2023-06-24 11:31:46 -0300

    main
    - Entrega Tarefa 01 - Murilo_Moscardi Brunna_Bettini Camili_Xi

---
+++ src/main/java/br/com/fatecararas/f290_ds2_controle_funcionarios/domain/entities/Funcionario.java
@@ -0,0 +1,73 @@
+package br.com.fatecararas.f290_ds2_controle_funcionarios.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Funcionario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "cargo_id_fk")
+    private Cargo cargo;
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario [id=" + id + ", nome=" + nome + ", email=" + email;
+    }
+    
+}
